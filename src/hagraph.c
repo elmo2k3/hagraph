32f6aaf92b6de3144f377d6905cbcd86b8c35050
Bjoern Biesenbach <bjoern@bjoern-b.de>
2009-03-10 10:56:57 +0100

    gpl added

--- src/hagraph.c
+++ src/hagraph.c
@@ -1,7 +1,20 @@
-/* Bjoern Biesenbach <bjoern@bjoern-b.de>
- * 
- * 
- * 
+/*
+ * Copyright (C) 2007-2009 Bjoern Biesenbach <bjoern@bjoern-b.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
 		
 		./hagraph -x 1300 -y 900 -f 2007-12-1 -t 2007-12-30 -g 2 -h 0 -i 2 -j 1 (Programm ausführen)
 
@@ -60,7 +73,7 @@ Module, Sensoren:
 
 #define MYSQL_HOST "bjoern-b.de"
 #define MYSQL_USER "weather_read"
-#define MYSQL_PASS "QPiEVtJ/6tLxQ"
+#define MYSQL_PASS ""
 #define MYSQL_PORT 3306
 #define MYSQL_DATABASE "home_automation"
 #define MYSQL_DATABASE_WS2000 "wetterstation"
